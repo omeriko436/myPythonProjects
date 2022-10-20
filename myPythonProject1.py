f78ddb94a09a8f1c06767ff1f386b79ce9600c60
omeriko436 <talhayeniyurt@gmail.com>
2022-10-20 20:25:32 +0300

    MPP1 2
    Wrote some code, but it isnt finished yet.

---
+++ myPythonProject1.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import time
+file = open("log.txt" , "a")
+paun = open("users.txt" , "r")
+print("")
+ent1 = input("Enter your username.")
+ent2 = input("Enter your password.")
+a = f.read(1)
+b = f.read(2)
+
+if  == usnm:
+    if ent2 == pasw:
+        print("You have sucsessfully entered in.")
+        print(" ")
+    else:
+        print("Your password is wrong.")
+else:
+    print("You entered a wrong username.")
